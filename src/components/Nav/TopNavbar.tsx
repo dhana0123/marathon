348df671d3b845250974b9398d71edbed36a49cc
dhana0123 <b.santhosh.dhana@gmail.com>
2022-12-21 17:40:07 +0530

    feat(topbarnav): topbarnav added

--- src/components/Nav/TopNavbar.tsx
+++ src/components/Nav/TopNavbar.tsx
@@ -1,14 +1,82 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Container, Stack, Box, Typography, Button } from "@mui/material";
+import {
+  Container,
+  Stack,
+  Box,
+  Typography,
+  Button,
+  useMediaQuery,
+  SwipeableDrawer,
+  IconButton,
+} from "@mui/material";
 import theme, { useTheme } from "@mui/material/styles";
+import ViewStreamIcon from "@mui/icons-material/ViewStream";
 
 const TopNavbar = () => {
   const { pathname } = useLocation();
   const theme = useTheme();
   const currrentPath = pathname.split("/")[1];
   const navigate = useNavigate();
-  console.log(currrentPath);
+  const matches = useMediaQuery("(max-width:600px)");
+  const [toggleNav, setToggleNav] = React.useState(false);
+
+  if (matches) {
+    return (
+      <Box p={2}>
+        <Stack
+          direction={"row"}
+          justifyContent="space-between"
+          alignItems={"center"}
+        >
+          <Box>Marathon</Box>
+          <IconButton
+            sx={{ backgroundColor: "grey.100" }}
+            onClick={() => setToggleNav(true)}
+          >
+            <ViewStreamIcon sx={{ color: "grey.600" }} />
+          </IconButton>
+        </Stack>
+        <SwipeableDrawer
+          anchor={"top"}
+          open={toggleNav}
+          onClose={() => setToggleNav(false)}
+          onOpen={() => setToggleNav(true)}
+        >
+          <Stack
+            direction={"column"}
+            justifyContent="center"
+            alignItems={"flex-start"}
+            spacing={2}
+            p={3}
+          >
+            <Typography
+              onClick={() => navigate("/plans")}
+              variant={currrentPath === "plans" ? "subtitle1" : "body1"}
+              sx={{
+                cursor: "pointer",
+                transition: theme.transitions.create(["fontSize"]),
+              }}
+            >
+              Pricing
+            </Typography>
+            <Typography
+              onClick={() => navigate("/contact")}
+              variant={currrentPath === "contact" ? "subtitle1" : "body1"}
+              sx={{
+                cursor: "pointer",
+                transition: theme.transitions.create(["fontSize"]),
+              }}
+            >
+              Contact
+            </Typography>
+            <Button variant="outlined">Login/Signup</Button>
+            <Button variant="contained">Start free trial</Button>
+          </Stack>
+        </SwipeableDrawer>
+      </Box>
+    );
+  }
 
   return (
     <Container sx={{ backgrouond: "transparent" }}>
